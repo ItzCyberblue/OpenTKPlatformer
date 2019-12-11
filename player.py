3d0eeacb8f592ebb5d1cbcf59895f4486d592613
IAmBot102102 <53452898+IAmBot102102@users.noreply.github.com>
2019-12-10 20:28:18 -0500

    Added New Files

---
+++ player.py
@@ -0,0 +1,6 @@
+class Player:
+	BoardX = 0
+	BoardY = 0
+	direction = "right"
+	Alive = True
+	body = [[BoardX, BoardY]]
---
+++ settings.py
@@ -0,0 +1,11 @@
+import pygame
+class Color:
+    BLACK = ( 0, 0, 0 )
+
+class Settings:
+    ScreenWidth = 800
+    ScreenHeight = 600
+    GameRunning = True
+    FPS = 60
+
+    
