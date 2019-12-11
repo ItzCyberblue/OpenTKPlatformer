ec8401f9dc2100c323b625a938421e8c795e7ef3
IAmBot102102 <53452898+IAmBot102102@users.noreply.github.com>
2019-12-10 19:37:16 -0500

    Project Setup Done

---
+++ OpenTKPlatformer/OpenTKPlatformer/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using OpenTK;
+
+
+namespace Program
+{
+    public class Game
+    {
+        int MaximumFramesPerSecond = 60;
+        private GameWindow window;
+        public Game(GameWindow window)
+        {
+            this.window = window;
+            Start();
+        }
+
+        void Start()
+        {
+            
+            window.Run(1/MaximumFramesPerSecond);
+        }
+    }
+}
---
+++ OpenTKPlatformer/OpenTKPlatformer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OpenTK;
+
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // JESSICA'S QUICK START PROGRAM
+            GameWindow window = new GameWindow(800, 600);
+            Game gameObject = new Game(window);
+        }
+    }
+}
