1eaaae5dbbdbe7f8f919d3fe6a4fc877ef062b87
XboxPro39 <57122545+XboxPro39@users.noreply.github.com>
2019-12-11 18:31:35 -0600

    ...

---
+++ GameScript.py
@@ -0,0 +1,44 @@
+import pygame
+import settings
+import player
+
+import backgroundObject
+
+
+SetttingsObject = settings.Settings()
+ColorObject = settings.Color()
+window = pygame.display.set_mode((SetttingsObject.ScreenWidth, SetttingsObject.ScreenHeight))
+pygame.display.set_caption("Pygame Game")
+
+backgroundObjects = [backgroundObject.BackgroundObject()]
+pygame.init()
+
+
+joysticks = []
+for i in range(0, pygame.joystick.get_count() - 1):
+    joysticks.append(pygame.joystick.Joystick())
+
+
+
+
+pygame.init()
+
+def MainLoop():
+    while SetttingsObject.GameRunning == True:
+    while SetttingsObject.GameRunning == True and PlayerObject.Alive == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                SetttingsObject.GameRunning = False
+        PlayerObject.getInput(backgroundObjects)
+
+        window.fill(ColorObject.BLACK)
+        pygame.draw.rect(window, ColorObject.RED, pygame.Rect((PlayerObject.PositionX, PlayerObject.PositionY), (PlayerObject.Width, PlayerObject.Height)))
+        for backgroundObj in backgroundObjects:
+            pygame.draw.rect(window, (255, 255, 255), pygame.Rect((backgroundObj.PositionX, backgroundObj.PositionY), (30, 40)))
+
+        pygame.display.flip()
+
+
+
+
+MainLoop()
